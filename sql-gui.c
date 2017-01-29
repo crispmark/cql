0f05741ba24342fe19be1ade82a4ddb2d1e2f987
crispmark <macr1324@gmail.com>
2017-01-29 14:51:42 -0500

    created a window that outputs text in textfield on button press

---
+++ sql-gui.c
@@ -0,0 +1,63 @@
+#include <gtk/gtk.h>
+#include <string.h>
+
+int main(int argc, char *argv[])
+{
+    GtkBuilder      *builder;
+    GtkWidget       *window;
+
+    gtk_init(&argc, &argv);
+
+    builder = gtk_builder_new();
+    gtk_builder_add_from_file (builder, "layout.glade", NULL);
+
+    window = GTK_WIDGET(gtk_builder_get_object(builder, "window_main"));
+    gtk_builder_connect_signals(builder, NULL);
+
+    g_object_unref(builder);
+
+    gtk_widget_show(window);
+    gtk_main();
+    return 0;
+}
+
+// called when window is closed
+void on_window_main_destroy()
+{
+    gtk_main_quit();
+}
+
+// called when run button is clicked
+void on_run_button_clicked(GtkWidget *button, gpointer user_data)
+{
+    GtkWidget *toplevel = gtk_widget_get_toplevel (button);
+    GtkWidget *child = gtk_bin_get_child(GTK_BIN(toplevel));
+    GList *children = gtk_container_get_children(GTK_CONTAINER(child));
+    for (GList *l = children; l != NULL; l = l->next)
+    {
+        const char *name = gtk_widget_get_name(l->data);
+        if (strcmp(name, "pane_2") == 0)
+        {
+            child = l->data;
+            break;
+        }
+    }
+    children = gtk_container_get_children(GTK_CONTAINER(child));
+    for (GList *l = children; l != NULL; l = l->next)
+    {
+        const char *name = gtk_widget_get_name(l->data);
+        if (strcmp(name, "main_text_view") == 0)
+        {
+            child = l->data;
+            break;
+        }
+    }
+    GtkTextIter start, end;
+    GtkTextBuffer *buffer = gtk_text_view_get_buffer ((GtkTextView*)child);
+    gchar *text;
+
+    gtk_text_buffer_get_bounds (buffer, &start, &end);
+    text = gtk_text_buffer_get_text (buffer, &start, &end, FALSE);
+    printf("%s\n", text);
+    g_free(text);
+}
